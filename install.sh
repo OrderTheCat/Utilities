1d2e88d612f9746265889ccdb2abfd267fa9e97f
OrderTheCat <nordvpn.gathering491@aleeas.com>
2026-05-12 19:26:05 +0530

    install script fixes + package installs

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 # Source
 
-DIR1=~/All-in-one-rice/topgrade.toml
+DIR1=~/Utilities/topgrade.toml
 
 # Destination
 
@@ -11,3 +11,43 @@ DEST1=~/.config
 mv "$DIR1" "$DEST1"
 
 echo "Setup of system utilities done!"
+
+#!/bin/bash
+
+# Figure out which distribution the user is on.
+
+echo "Select your Linux distribution:"
+echo "1) Fedora"
+echo "2) Debian/Ubuntu"
+echo "3) Arch Linux"
+
+read -p "Enter choice [1-3]: " choice
+
+PACKAGES="fastfetch kitty firefox"
+
+case $choice in
+    1)
+        echo "Detected Fedora"
+        sudo dnf update -y
+        sudo dnf install -y $PACKAGES
+        ;;
+    
+    2)
+        echo "Detected Debian/Ubuntu"
+        sudo apt update
+        sudo apt install -y $PACKAGES
+        ;;
+    
+    3)
+        echo "Detected Arch Linux"
+        sudo pacman -Syu --noconfirm
+        sudo pacman -S --noconfirm $PACKAGES
+        ;;
+    
+    *)
+        echo "Invalid choice."
+        exit 1
+        ;;
+esac
+
+echo "Installation complete!"
